b1ba121b42aae5cb8c00f0fc2af85409197fbf9d
gamminzo <buddiesgamingwith59@gmail.com>
2024-07-09 14:58:07 +0545

    pythons loops

---
+++ loop.py
@@ -0,0 +1,51 @@
+#lis=(1,4,9,16,25,36,49,64,81,100)
+#x=49
+#i=0
+#while i<len(lis):
+#    if (lis[i]==x):
+#        print("Number found in",i)
+#    else:
+#    i+=1
+
+#i=1
+#while i<=10:
+  ##     i+=1
+    #    continue
+    #print(i)
+    #i+=1
+
+#hello="Mansih bhugai"
+#for char in hello:
+ #   if(char=="i"):
+ #       print("o is found")
+  #      break
+   # print(char)
+#else:
+ #   print("end")
+
+#num=(1,4,9,16,25,36,49,64,81,100)
+#x=25
+#idx=0
+#for char in num:
+#    if(char==x):
+ #   
+  #      print("x is found in ",idx)
+   # idx+=1    
+#n=5
+#for i in range(1,11):
+#    pass
+#print(1)
+
+#x=7
+#i=1
+#sum=0
+#while(i<=x):
+ #   sum=sum+i
+  #  i+=1
+
+#print(sum)    
+#n=5
+#tor=1
+#for i in range(1,n+1):
+ #   tor*=i
+#print(tor)    
