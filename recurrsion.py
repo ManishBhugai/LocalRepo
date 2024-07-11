7ebb7a37a37542d3e4da8005ee10b8172d832384
gamminzo <buddiesgamingwith59@gmail.com>
2024-07-11 23:02:27 +0545

    recursion

---
+++ recurrsion.py
@@ -0,0 +1,30 @@
+# def show(n):  
+#if(n==1 or n==0):
+ #       return 1
+   # return show(n-1)+n
+
+#print(show(5))
+
+#def print_list(list, idx=0):
+ #   if(idx==len(list)):
+  #      return
+   # print(list[idx])
+   # print_list(list, idx+1)
+
+#fruits=["mango", "apple", "banana", "orange"]
+
+#print_list(fruits)
+
+
+
+
+
+#def ele(list,I=0):
+ #   if(I==len(list)):
+ #       return
+  #  print(list[I])
+   # ele(list, I+1)
+
+#fruit=["mang0", "baanana", "apple", "ball"]
+ 
+#ele(fruit)
