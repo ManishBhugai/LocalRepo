74e768668017a899257274abb24d54b29abf468d
gamminzo <buddiesgamingwith59@gmail.com>
2024-07-12 12:40:46 +0545

    function

---
+++ function.py
@@ -0,0 +1,38 @@
+#subject = ["science", "maths", "chemstery", "science"]
+
+#def print_len(list):
+#    print(len(list))
+
+#print_len(subject)
+
+#def print_element(list):
+ #   for i in list:
+  #      print(i,end=" ")
+#print_element(subject)
+
+
+#def print_fac(n):
+   # fact=1
+   # for i in range(1,n+1):
+   #     fact*=i
+   # print(fact)
+       
+#print_fac(5)
+
+#def print_conv(N):
+  #  i= N*87
+   # print(N,"usd=",i,"INR")
+ 
+#print_conv(5)
+
+#def odd(n):
+ #   if(n%2==0):
+  #      print("evem")
+   # else:
+   #     print("odd")
+
+#odd(int(input("enter a number"))) 
+
+
+  
+ 
